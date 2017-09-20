7b6c9ee56d459a3f4db21451c335af23aa3f30b4
Benjamin Leber <grisu118@grisu118.ch>
2017-09-20 19:25:30 +0200

    + Update excludes in deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ if [ -f $ZIPNAME ]; then
 fi
 
 echo "Create new zip"
-"C:\Program Files\7-Zip\7z.exe" a -x!*.sh -x!.git -x!LICENSE -x!*.zip -x!*.iml -x!.idea -tzip $ZIPNAME *
+"C:\Program Files\7-Zip\7z.exe" a -x!*.sh -x!.git -x!LICENSE -x!*.zip -x!*.iml -x!.idea -x!*.md -x!*.pdn -x!*.png -tzip $ZIPNAME *
 
 echo "Copy to mods directory"
 cp -f $ZIPNAME "$MOD_DIR"
